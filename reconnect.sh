84c52561c1ee381177236966a5b6f0ddc719f6d1
chrisfavila-aic <chrisfavila@ateneoinnovation.org>
2017-08-16 22:40:27 +0800

    use Not condition in reconnect.sh

--- reconnect.sh
+++ reconnect.sh
@@ -1,6 +1,6 @@
 source /home/pi/rainlogger.conf
 
-if [ "$(hostname -I | grep 172)" ]; then
+if ! [ "$(hostname -I | grep 192)" ]; then
     if [ "$(iwlist wlan0 scan | grep $SSID_SITE)" ] || [ "$(iwlist wlan0 scan | grep $SSID_MOBILE)" ]; then
         ifdown wlan0
         ifup wlan0;
